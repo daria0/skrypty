4c36b0fb063a892cd2a06af241c1de33cd9e6d91
daria <dariasiemieniuk@gmail.com>
2021-04-24 19:39:29 +0200

    Added color variables

--- pygame-arkanoid/game_settings.py
+++ pygame-arkanoid/game_settings.py
@@ -13,9 +13,22 @@ GAME_LVL = 1
 
 ball_speed = 1
 
+# FLAGS
+
 running = True
 paused = False
 game_lost = False
 game_won = False
 lvl_won = False
 display_menu = True
+
+# COLORS
+
+light_blue = (143, 186, 207)
+light_grey = (166, 166, 166)
+dark_grey = (50, 50, 50)
+green = (0, 255, 0)
+red = (255, 0, 0)
+light_orange = (224, 207, 177)
+white = (255, 255, 255)
+black = (0, 0, 0)
